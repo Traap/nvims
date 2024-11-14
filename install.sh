b4b9e70cc24c6c99b63fbc1f0cd9579d49ac1c85
Traap <gary.a.howard@mac.com>
2024-11-13 21:19:52 -0800

    fix: ~/.config/nvims/default_config obsoleted.

--- install.sh
+++ install.sh
@@ -11,7 +11,6 @@ sudo chmod -v +x /usr/local/bin/nvims
 # Create config directory and copy files there.
 mkdir -p "$HOME"/.config/nvims
 cp -v neovim_distros "$HOME"/.config/nvims/.
-touch "$HOME"/.config/nvims/default_config
 
 # Cleanup temporary directory.
 rm -rfv /tmp/traap
